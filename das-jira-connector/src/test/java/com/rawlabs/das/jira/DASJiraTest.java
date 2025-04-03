5f21bf1290f4998239d9b133acdd0b347da5c509
Benjamin Gaidioz <ben@raw-labs.com>
2025-04-03 17:56:18 +0200

    Fixed test

--- das-jira-connector/src/test/java/com/rawlabs/das/jira/DASJiraTest.java
+++ das-jira-connector/src/test/java/com/rawlabs/das/jira/DASJiraTest.java
@@ -2,11 +2,8 @@ package com.rawlabs.das.jira;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.rawlabs.das.jira.tables.definitions.DASJiraAdvancedSettingsTable;
-import com.rawlabs.das.sdk.DASSdk;
-import com.rawlabs.das.sdk.DASTable;
+import com.rawlabs.das.sdk.DASSdkInvalidArgumentException;
 import java.util.Map;
-import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,13 +15,34 @@ public class DASJiraTest {
     DASJiraBuilder dasJiraBuilder = new DASJiraBuilder();
     assertNotNull(dasJiraBuilder);
     assertEquals("jira", dasJiraBuilder.getDasType());
-    DASSdk dasJira =
-        dasJiraBuilder.build(Map.of("base_url", "test_url", "personal_access_token", "pat"), null);
-    assertNotNull(dasJira);
-    assertInstanceOf(DASJira.class, dasJira);
-    int tableDefinitionsCount = dasJira.getTableDefinitions().size();
-    assertNotEquals(0, tableDefinitionsCount);
-    Optional<DASTable> tablesCount = dasJira.getTable(DASJiraAdvancedSettingsTable.TABLE_NAME);
-    assertTrue(tablesCount.isPresent());
+  }
+
+  @Test
+  public void testMalformedUrl() {
+    DASJiraBuilder dasJiraBuilder = new DASJiraBuilder();
+    DASSdkInvalidArgumentException exception =
+        assertThrows(
+            DASSdkInvalidArgumentException.class,
+            () ->
+                dasJiraBuilder.build(
+                    Map.of("base_url", "tralala", "personal_access_token", "pat"), null));
+    assertTrue(exception.getMessage().contains("no scheme was found"), exception.getMessage());
+  }
+
+  @Test
+  public void testUnknownHost() {
+    DASJiraBuilder dasJiraBuilder = new DASJiraBuilder();
+    DASSdkInvalidArgumentException exception =
+        assertThrows(
+            DASSdkInvalidArgumentException.class,
+            () ->
+                dasJiraBuilder.build(
+                    Map.of(
+                        "base_url",
+                        "https://this-doesn-t-exist.com",
+                        "personal_access_token",
+                        "pat"),
+                    null));
+    assertTrue(exception.getMessage().contains("this-doesn-t-exist.com: nodename nor servname provided, or not known"), exception.getMessage());
   }
 }
